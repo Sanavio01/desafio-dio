9c1c66bbf2f456261e7b3ebd64a25002c93e6ff1
Sanavio01 <sanavioguitar@gmail.com>
2023-12-24 17:59:45 -0300

    codigo completo

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let nome = 'Batman';
 let nivel = 100001;
-if (nivel <= 1000){
-    console.log('O Heroi ' + nome + ' esta no nivel Ferro');}
-   else if (nivel > 1000 && nivel <= 2000)
-   { console.log('O Heroi ' + nome + ' esta no nivel Bronze');}
-    else if (nivel > 2000 , nivel <= 5000)
-   { console.log('O Heroi ' + nome + ' esta no nivel Prata');}
-    else if (nivel > 5000 , nivel <= 7000)      
-    { console.log('O Heroi ' + nome + ' esta no nivel Ouro');}
-    else if (nivel > 7000 , nivel <= 8000)
-    { console.log('O Heroi ' + nome + ' esta no nivel Platina');}
-    else if (nivel > 8000 , nivel <= 9000)
-    { console.log('O Heroi ' + nome + ' esta no nivel Ascendente');}
-    else if (nivel > 9000 , nivel <= 10000)
-    { console.log('O Heroi ' + nome + ' esta no nivel Imortal');}
-    else 
-    { console.log('O Heroi ' + nome + ' esta no nivel Radiante');}
-    
+
+if (nivel <= 1000) {
+    console.log('O Herói ' + nome + ' está no nível Ferro');
+} else if (nivel > 1000 && nivel <= 2000) {
+    console.log('O Herói ' + nome + ' está no nível Bronze');
+} else if (nivel > 2000 && nivel <= 5000) {
+    console.log('O Herói ' + nome + ' está no nível Prata');
+} else if (nivel > 5000 && nivel <= 7000) {
+    console.log('O Herói ' + nome + ' está no nível Ouro');
+} else if (nivel > 7000 && nivel <= 8000) {
+    console.log('O Herói ' + nome + ' está no nível Platina');
+} else if (nivel > 8000 && nivel <= 9000) {
+    console.log('O Herói ' + nome + ' está no nível Ascendente');
+} else if (nivel > 9000 && nivel <= 10000) {
+    console.log('O Herói ' + nome + ' está no nível Imortal');
+} else {
+    console.log('O Herói ' + nome + ' está no nível Radiante');
+}
 
 
