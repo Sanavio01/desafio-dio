10a2389c0a957a626aae177aada63947a9e90e05
Sanavio01 <sanavioguitar@gmail.com>
2023-12-24 17:53:23 -0300

    codigo completo

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+let nome = 'Batman';
+let nivel = 100001;
+if (nivel <= 1000){
+    console.log('O Heroi ' + nome + ' esta no nivel Ferro');}
+   else if (nivel > 1000 && nivel <= 2000)
+   { console.log('O Heroi ' + nome + ' esta no nivel Bronze');}
+    else if (nivel > 2000 , nivel <= 5000)
+   { console.log('O Heroi ' + nome + ' esta no nivel Prata');}
+    else if (nivel > 5000 , nivel <= 7000)      
+    { console.log('O Heroi ' + nome + ' esta no nivel Ouro');}
+    else if (nivel > 7000 , nivel <= 8000)
+    { console.log('O Heroi ' + nome + ' esta no nivel Platina');}
+    else if (nivel > 8000 , nivel <= 9000)
+    { console.log('O Heroi ' + nome + ' esta no nivel Ascendente');}
+    else if (nivel > 9000 , nivel <= 10000)
+    { console.log('O Heroi ' + nome + ' esta no nivel Imortal');}
+    else 
+    { console.log('O Heroi ' + nome + ' esta no nivel Radiante');}
+    
+
+
